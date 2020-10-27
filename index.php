0c72fb915516347a071d9e637e5f6173622f6f8e
NiSilvestrini <silvestrinih03@gmail.com>
2020-10-27 09:23:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
+
 <?php
 
-echo 'Hello World';
+echo 'Hello World <br>';
 
-//var: ( $nome = valor; )
+
+//var: ( $nomevar = valor; )
 
     $nome='Nic'; //var nome tem valor igual a Nic
     $idade='17';
-    echo $nome; //mostra na tela 'Nic'
-    echo $idade;
+    echo $nome . '<br>'; //mostra na tela 'Nic'
+    echo $idade . '<br>';
 
 //var: Números
 
@@ -17,15 +19,52 @@ echo 'Hello World';
     $var4 = $var3*5;
     $var5 = 10/2;
 
-    echo 'var 1 igual á: ' . $var1;
+    echo 'var 1 igual á: ' . $var1 . '<br>';
 
-    echo 'var 2 igual á: ' . $var2;
+    echo 'var 2 igual á: ' . $var2. '<br>';
 
-    echo 'var 3 igual á: ' . $var3;
+    echo 'var 3 igual á: ' . $var3. '<br>';
 
-    echo 'var 4 igual á: ' . $var4;
+    echo 'var 4 igual á: ' . $var4. '<br>';
 
-    echo 'var 5 igual á: ' . $var5;
+    echo 'var 5 igual á: ' . $var5. '<br>';
+
+//var: strings
+
+    $nome = 'Nicole';
+    $sobrenome = 'Silvestrini';
+    echo $nome . ' ' . $sobrenome . '<br>';//Nicole Silvestrini
+
+//concatenação = .
+//aspas simples dão mais velocidade
+
+//Condicionais:
+
+    $var1 = 3;
+    $var2 = 2;
+
+    if($var1 + $var == 5){
+            echo $var1 .' + '. $var2 .' é igual a 5';
+    }   else{
+            echo $var1 .' + '. $var2 .'é diferente de 5';
+    }
+
+
+//Se (if) var1 + var2 for 5 recebe a mensagem do 1º echo, senão (else) recebe a mensagem do 2º echo;
+
+
+    $var = 5;
+    if($var > 10){
+        echo 'A var '. $var .' é maior que 10';
+    } else if($var > 7){
+        echo 'A var ' . $var . ' é maior que 7';
+    }else if($var>=5){
+        echo 'A var ' . $var . ' é maior ou igual a 5'; //cód executado
+    }else if($var>=3){
+        echo 'A var ' .$var. ' é maior ou igual a 3';
+    }else{
+        echo 'A var $var possui um valor diferente das condicionais anteriores';
+    }
 
 
 
